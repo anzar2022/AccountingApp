2c7c4adb4fb71969668f50f41fcdaa0f09816aa0
Anzar Ansari <anzar.ansari@wonderbiz.in>
2023-12-15 14:37:32 +0530

    added field in GetAccountDto

--- AccountApi/Dtos/AccountDto.cs
+++ AccountApi/Dtos/AccountDto.cs
@@ -2,7 +2,7 @@
 
 namespace AccountApi.Dtos
 {
-    public record GetAccountDto(Guid Id, string AccountName, string MobileNo, string EmailAddress, bool IsActive);
+    public record GetAccountDto(Guid Id, string AccountName, string MobileNo, string EmailAddress, string PanCard, string AdharCard, string Adderss, bool IsActive, DateTime CreatedDate);
     public record CreateAccountDto([Required(ErrorMessage ="Account name is required")] string AccountName, [Required(ErrorMessage = "Mobile No is required")] string MobileNo, string EmailAddress, bool IsActive);
     // public record UpdateAccountDto(Guid Id , string AccountName, string MobileNo, string EmailAddress, bool IsActive);
 
