c285617fb96c59c520bbfc47ab155a4ace769a02
Ved Thakur <SESA744803@se.com>
2023-12-27 10:16:56 +0530

    Changes

--- TransactionApi/Dtos/InterestEMIDto.cs
+++ TransactionApi/Dtos/InterestEMIDto.cs
@@ -2,7 +2,7 @@
 {
     public class InterestEMIDto
     {
-        public record GetInterestEMIDto(Guid Id, Guid TransactionId, double PrincipalAmount, double InterestRate, double InterestAmount, double PaidInterestAmount, string emiMonth);
+        public record GetInterestEMIDto(Guid Id, Guid TransactionId, double PrincipalAmount, double InterestRate, double InterestAmount, double BalanceInterestAmount, double PaidInterestAmount, string emiMonth);
         public record UpdateInterestEMIDto(Guid Id, double PaidInterestAmount);
         public record GenerateInterestEMIDto(Guid TransactionId, string EmiMonth);
 
